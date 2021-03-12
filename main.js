3387c19ce6581e714ee2b53d0b2ec9742ae811e3
Hazieon <handrew3721@gmail.com>
2021-03-12 16:42:15 +0000

    GAME COMPLETE. basic functionality complete

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let state = {
 function nextdress() {
   console.log("nextdress triggered");
   console.log(state.dress);
-  const dress = document.querySelector("#dress");
+  let dress = document.querySelector("#dress");
   //code from the tutorial example:
   //   if (state.dress === 0) {
   //     dress.setAttribute("class", "dress1");
@@ -25,12 +25,45 @@ function nextdress() {
   //     state.dress = 0;
   //     console.log(state);
   //   }
-  //my refactored version with LESS code and MORE scalability
+
+  //my refactored version with LESS code and MORE scalability:
   if (state.dress < 3) {
     state.dress++;
     dress.setAttribute("class", `dress${state.dress}`);
   } else if (state.dress === 3) {
     state.dress = 0;
     dress.setAttribute("class", `dress${state.dress}`);
+  }
+}
+
+function nextshoes() {
+  let shoes = document.querySelector("#shoes");
+  if (state.shoes < 2) {
+    state.shoes++;
+    shoes.setAttribute("class", `shoes${state.shoes}`);
+  } else if (state.shoes === 2) {
+    state.shoes = 0;
+    shoes.setAttribute("class", `shoes${state.shoes}`);
+  }
+}
+function nexthair() {
+  let hair = document.querySelector("#hair");
+  if (state.hair < 2) {
+    state.hair++;
+    hair.setAttribute("class", `hair${state.hair}`);
+  } else if (state.hair === 2) {
+    state.hair = 0;
+    hair.setAttribute("class", `hair${state.hair}`);
+  }
+}
+
+function nexthat() {
+  let hat = document.querySelector("#hat");
+  if (state.hat < 2) {
+    state.hat++;
+    hat.setAttribute("class", `hat${state.hat}`);
+  } else if (state.hat === 2) {
+    state.hat = 0;
+    hat.setAttribute("class", `hat${state.hat}`);
   }
 }
